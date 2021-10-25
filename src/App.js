980780ecd1bf1078327ae38281358c1ce69b1c23
Wesley <wesley.brazil@outlook.com>
2021-10-25 13:03:55 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import cronometro from './assets/cronometro.png';
+import './assets/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'VAI'
+    };
+    this.timer = null;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this)
+  };
+
+  vai(){
+    let state = this.state;
+
+    if(this.timer !== null){
+
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = 'VAI';
+    
+    } else {
+      
+      this.timer = setInterval(()=>{
+        state.numero += 0.1;
+        this.setState(state)
+      }, 100);
+      state.botao = 'PAUSAR';
+
+    }
+
+
+    this.setState(state)
+  };
+
+  limpar(){}
+
+  render(){
+    return(
+      <div className='container'>
+        <img src={cronometro} alt='cronometro'/>
+        <a className='timer'> {this.state.numero.toFixed(1)} </a>
+        <div className='areaBtn'>
+          <p className='botao' onClick={this.vai}> {this.state.botao} </p>
+          <p className='botao' onClick={this.limpar}> LIMPAR </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
